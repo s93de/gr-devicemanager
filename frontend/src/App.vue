773bbdd4fe8c96f9f5b29b8e54139af4fc851078
Udo Schwab <dev@udoschwab.eu>
2020-07-18 22:51:29 +0200

    Tidied up

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,35 +5,9 @@
 </template>
 
 <script>
-import axios from 'axios';
-//import Dashboard from './components/Dashboard.vue';
-//import Details from './components/Details';
 
 export default {
-  name: 'App',
-  components: {
-    //Dashboard,
-    //Details
-  },
-  data:function(){
-    return{
-      detailID:15,
-      machineItem:null
-    }
-  },
-  methods:{
-    load:function(){
-      var url = 'http://localhost:5001/api/machine/'+this.detailID;
-      axios.get(url)
-      .then(resp=>this.machineItem=resp.data)
-      .catch(function(){
-        console.log("Can't find machine!");
-      })
-    }
-  },
-  mounted:function(){
-    this.load();
-  }
+  name: 'App'
   
 }
 </script>
